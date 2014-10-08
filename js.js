24c6cbaf325253391907f67d5fda767075b3c194
Marina <velikajaweb@gmail.com>
2014-10-09 00:37:30 +0400

    Landing page - Crimean Trip
    Landing page - Crimean Trip

---
+++ js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+             
+        $(".opacity, .close, #parent_popup").click(function() {
+          $(".callback-form, #parent_popup").hide();
+          $(".opacity").hide();
+        });
+   
+});
